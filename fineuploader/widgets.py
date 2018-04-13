c6e03f713af0c171b52f8494f8c35ce0c4b66c47
Basil Shubin <basil.shubin@gmail.com>
2018-04-13 14:39:24 +0700

    code clean-up…

--- fineuploader/widgets.py
+++ fineuploader/widgets.py
@@ -5,6 +5,8 @@ import json
 from django.forms import ClearableFileInput
 from django.contrib.contenttypes.models import ContentType
 
+from attachments.models import Attachment
+
 
 class FineInput(ClearableFileInput):
     template_name = 'fineuploader/fineinput.html'
@@ -27,3 +29,6 @@ class FineInput(ClearableFileInput):
             'object': target_object,
         })
         return self._render(self.template_name, context, renderer)
+
+    def value_from_datadict(self, data, files, name):
+        return Attachment.objects.attachments_for_object(self.target_object) or None
